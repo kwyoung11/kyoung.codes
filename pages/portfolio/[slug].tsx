a17f50148438abf4a2e219109c964f61f770e147
Kevin Young <kyoung18@terpmail.umd.edu>
2020-04-18 16:57:45 -0500

    fix: updates ordering on portoflio page, adds source link, adds typings

--- pages/portfolio/[slug].tsx
+++ pages/portfolio/[slug].tsx
@@ -4,22 +4,25 @@ import jdown from 'jdown/dist';
 import { BlogPostLayout } from '../../src/components/Layout/BlogPost/BlogPostLayout';
 import { parseEntry } from '../../src/util/helpers';
 import fs from 'fs';
-import { Post } from '../../src/components/Blog/Post';
 
 const PortfolioTemplate = (props) => {
   return (
     <BlogPostLayout config={props.config}>
-      <Post post={props.post} />
+      <div>Hello, world!</div>
     </BlogPostLayout>
   );
 };
 
 export async function getStaticProps({ params }) {
+  console.log('getStaticProps', params);
   const contentDirectory = path.join(process.cwd(), 'content');
   const dataDirectory = path.join(process.cwd(), 'data');
   const config = JSON.parse(fs.readFileSync(`${dataDirectory}/config.json`, 'utf8'));
-  const content = fs.readFileSync(`${contentDirectory}/blog/${params.slug}.md`, 'utf8');
-  const contentObj = parseEntry(params.slug, content);
+  const content = fs.readFileSync(
+    `${contentDirectory}/portfolio/${params.portfolioSlug}.md`,
+    'utf8',
+  );
+  const contentObj = parseEntry(params.portfolioSlug, content);
 
   return {
     props: {
@@ -31,15 +34,16 @@ export async function getStaticProps({ params }) {
 
 export async function getStaticPaths() {
   const routes = [];
-  const blogPosts = await jdown('./content/blog');
+  const portfolioPosts = await jdown('./content/portfolio');
 
   // add each blog to the routes obj
-  Object.entries(blogPosts).forEach(([filename, fileContent]) => {
+  Object.entries(portfolioPosts).forEach(([filename, fileContent]) => {
     const filen = filename.replace(/([a-z][A-Z])/g, function (g) {
       return g[0] + '-' + g[1].toLowerCase();
     });
-    routes.push({ params: { slug: filen } });
+    routes.push({ params: { portfolioSlug: filen } });
   });
+  console.log('ROUTES', routes);
   return {
     paths: routes,
     fallback: false,
